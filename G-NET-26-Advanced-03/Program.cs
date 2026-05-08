3f6043c63eb5366505e07d452920c5661814d520
Mazen Thabet <MazenThabet2@outlook.com>
2026-05-08 23:53:02 +0300

    Add project files.

---
+++ G-NET-26-Advanced-03/Program.cs
@@ -0,0 +1,32 @@
+﻿#region using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+#endregion
+namespace G_NET_26_Advanced_03;
+    public class Program { 
+        public static void Main(string[] args) {
+        #region Ex1: Student Grade Manager
+        //1
+            List<int> Grades = new List<int> { 85, 92, 78, 95, 88, 70, 100, 65 };
+            Console.WriteLine("Grades:"); 
+            for (int i = 0; i < Grades.Count; i++) 
+            Console.WriteLine($"Grade {i + 1}: {Grades[i]}");
+            Console.WriteLine($"Total count is: {Grades.Count}");
+            Console.WriteLine($"First Grade is: {Grades.First()}");
+            
+
+
+
+
+
+        #endregion
+
+
+    }
+} 
+
